10259f88026ef08ddb2c6a85b5ef58c79d415ee0
kenway214 <kenway214@outlook.com>
2023-10-01 00:12:29 +0530

    ruby: EvolutionX-ify

---
+++ vendorsetup.sh
@@ -0,0 +1,16 @@
+echo 'Starting to clone stuffs needed to build for ruby '
+
+
+# Vendor
+echo 'Cloning vendor tree'
+rm -rf vendor/xiaomi/ruby  && git clone https://github.com/PQEnablers-Devices/android_vendor_xiaomi_ruby -b lineage-20 vendor/xiaomi/ruby
+
+# Kernel
+echo 'Cloning kernel tree'
+rm -rf kernel/xiaomi/mt6877 && git clone --depth=1 https://github.com/PQEnablers-Devices/android_kernel_xiaomi_mt6877 -b lineage-20 kernel/xiaomi/mt6877
+
+# Xiaomi hardware
+echo 'Cloning hardware xiaomi'
+rm -rf hardware/mediatek && git clone https://github.com/PQEnablers-Devices/android_hardware_mediatek -b lineage-20-foss hardware/mediatek
+
+echo 'delete vendorsetup.sh from device tree once this is done'
